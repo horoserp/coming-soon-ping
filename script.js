a44b5ac80ebc968b26374395e2d386b9c6973189
Robert Horosewski <horoserp@yahoo.com>
2025-03-14 22:23:39 -0400

    Update: Complete JS for error handling on input.

---
+++ script.js
@@ -0,0 +1,20 @@
+const email = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
+
+email.addEventListener("invalid", postError);
+email.addEventListener("change", clearError);
+
+function postError() {
+  email.classList.add("error");
+  if (email.value === "") {
+    errorMessage.innerHTML = "Required!";
+  } else {
+    console.log("Error");
+    errorMessage.innerHTML = "Please provide a valid email address";
+  }
+}
+
+function clearError() {
+  email.classList.remove("error");
+  errorMessage.innerHTML = "";
+}
